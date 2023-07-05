097e77999d5ea586911ce7ba7672411fd925c887
Deepak Singh <deepak.singh@growthnatives.co>
2023-07-05 13:58:44 +0530

    conditional layout using array

--- src/app/login/layout.js
+++ src/app/login/layout.js
@@ -4,11 +4,14 @@ import { usePathname } from 'next/navigation'
 
 const LoginLayout = ({children}) => {
   const pathName = usePathname();
+  const exclusionArray = ['/login/student_login']
+
+  let isLoginLayoutVisible = exclusionArray.indexOf(pathName) < 0;
   console.log("pathname == ",pathName);
   return (
     <>
     {
-      pathName !== '/login/student_login' && 
+      isLoginLayoutVisible && 
       <div style={{textAlign:'center'}}>
       <h2>hello, I am login layout</h2>
       </div>
